d7ed3fc14f33f471689a1ef977647c7a97b876fd
Items0 <nieznany012@wp.pl>
2018-03-10 23:43:09 +0100

    Sinogram correction

--- tomograf.py
+++ tomograf.py
@@ -5,9 +5,14 @@ import cmath
 from math import *
 from skimage.transform import radon
 
-def line(x0,y0, x1,y1, zdj):
-    suma = 0
-    ile = 0
+def calculateAvg(myLine, image):
+    sum = 0
+    for y, x in myLine:
+        sum += image[y,x]
+    return sum / len(myLine)
+
+def line(x0,y0, x1,y1):
+    myLine = []
     dx = x1-x0
     dy = y1-y0
 
@@ -26,12 +31,9 @@ def line(x0,y0, x1,y1, zdj):
         d = 2 * dy - dx
         delta_A = 2 * dy
         delta_B = 2 * dy - 2 * dx
-
         x, y = (0, 0)
         for i in range(dx+1):
-            #zdj[y0+y][x0+x] = 1
-            suma += zdj[y0+y][x0+x]
-            ile += 1
+            myLine.append([y0 + y, x0 + x])
             if d > 0:
                 d += delta_B
                 x += inc_x
@@ -44,12 +46,9 @@ def line(x0,y0, x1,y1, zdj):
         d = 2 * dx - dy
         delta_A = 2 * dx
         delta_B = 2 * dx - 2 * dy
-
         x, y = (0, 0)
         for i in range(dy+1):
-            #zdj[y0 + y][x0 + x] = 1
-            suma += zdj[y0 + y][x0 + x]
-            ile += 1
+            myLine.append([y0 + y, x0 + x])
             if d > 0:
                 d += delta_B
                 x += inc_x
@@ -57,7 +56,7 @@ def line(x0,y0, x1,y1, zdj):
             else:
                 d += delta_A
                 y += inc_y
-    return suma/ile
+    return myLine
 
 def main():
     fileName = "Kwadraty2.jpg"
@@ -76,13 +75,10 @@ def main():
     for i in range(0, krok):
         xe = int(round(r * cos(radians(alfa * i)) + center[1], 1))
         ye = int(round(r * sin(radians(alfa * i)) + center[0], 1))
-        #image[ye][xe] = 1
         for k in range(0, ndetektorow):
-            xdi = int(round(r * cos(radians(alfa + 180 - rozpietosc / 2 + k * rozpietosc / (ndetektorow - 1))), 1) + center[1])
-            ydi = int(round(r * sin(radians(alfa + 180 - rozpietosc / 2 + k * rozpietosc / (ndetektorow - 1))), 1) + center[0])
-            tab[i][k] = line(xe, ye, xdi, ydi, image)
-            #image[ydi][xdi] = 1
-    #print(tab)
+            xdi = int(round(r * cos(radians(alfa * i + 180 - rozpietosc / 2 + k * rozpietosc / (ndetektorow - 1))), 1) + center[1])
+            ydi = int(round(r * sin(radians(alfa * i + 180 - rozpietosc / 2 + k * rozpietosc / (ndetektorow - 1))), 1) + center[0])
+            tab[i][k] = calculateAvg(line(xe, ye, xdi, ydi), image)
     '''
     mymax = np.max(tab)
     for i in range(0, tab.shape[0]):
@@ -90,11 +86,8 @@ def main():
             tab[i][k] = tab[i][k] / mymax
     print(tab)
     '''
-    #print(image)
     io.imshow(tab)
-    #theta = np.linspace(0., 180., max(image.shape), endpoint=False)
-    #sinogram = radon(image, theta=theta, circle=True)
-    #io.imshow(sinogram, cmap=plt.cm.Greys_r, extent=(0, 180, 0, sinogram.shape[0]), aspect='auto')
     plt.show()
+
 if __name__ == '__main__':
     main()
